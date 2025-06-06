06cf490535b79e6eb74467d11b6617ce12fc258a
Justin Chang <chang.justin0106@gmail.com>
2025-06-05 21:25:07 -0700

    Changed document title

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import LandingPage from "./LandingPage"
 import Video from "./Video"
 
 function App() {
+  document.title = "MotionLink - A Cloud-Based, Motion Detection Security System"
+
   return (
     <div className="h-screen w-full snap-y snap-mandatory overflow-y-scroll">
       <LandingPage/>
